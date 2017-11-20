0121c4c73389b23a0b1590efd81e95f114d13732
Kanglai Qian <qiankanglai@gmail.com>
2017-11-20 10:07:13 +0800

    tab to space

--- source/downloads/code/CubemapCaptureEditor.cs
+++ source/downloads/code/CubemapCaptureEditor.cs
@@ -4,26 +4,26 @@ using System.Collections;
 
 [CustomEditor(typeof(CubemapCaptureScript))]
 public class CubemapCaptureEditor : Editor {
-	void OnEnable() {
-		CubemapCaptureScript myScript = (CubemapCaptureScript)target;
-		myScript.Setup ();
-	}
+    void OnEnable() {
+        CubemapCaptureScript myScript = (CubemapCaptureScript)target;
+        myScript.Setup ();
+    }
 
-	void OnDisable() {
-		CubemapCaptureScript myScript = (CubemapCaptureScript)target;
-		myScript.Cleanup ();
-	}
+    void OnDisable() {
+        CubemapCaptureScript myScript = (CubemapCaptureScript)target;
+        myScript.Cleanup ();
+    }
 
-	public override void OnInspectorGUI()
-	{
-		DrawDefaultInspector();
-		
-		CubemapCaptureScript myScript = (CubemapCaptureScript)target;
-		myScript.TextureSize = EditorGUILayout.IntField ("Texture Size", myScript.TextureSize);
-		myScript.PreviewCamera = EditorGUILayout.Toggle ("Preview", myScript.PreviewCamera);
-		if(GUILayout.Button("Build Cubemap"))
-		{
-			myScript.BuildCubemap();
-		}
-	}
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        CubemapCaptureScript myScript = (CubemapCaptureScript)target;
+        myScript.TextureSize = EditorGUILayout.IntField ("Texture Size", myScript.TextureSize);
+        myScript.PreviewCamera = EditorGUILayout.Toggle ("Preview", myScript.PreviewCamera);
+        if(GUILayout.Button("Build Cubemap"))
+        {
+            myScript.BuildCubemap();
+        }
+    }
 }
